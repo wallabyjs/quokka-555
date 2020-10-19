7a8b861ccd3d6191e6f84ee6b4ec4e1ee26ad22b
Simon McEnlly <simon.mcenlly@gmail.com>
2020-10-20 08:12:26 +1000

    Initial Commit

---
+++ src/ha/product.service.ts
@@ -0,0 +1,7 @@
+import { Injectable, NotFoundException, HttpService , Logger, NotImplementedException } from '@nestjs/common';
+
+console.log(Injectable);
+console.log(NotFoundException);
+console.log(HttpService);
+console.log(Logger);
+console.log(NotImplementedException);
